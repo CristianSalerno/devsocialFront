b1ed6784b081149e851e0419ba43a10644f8dfee
Cristian Salerno <cristiansalerno18@gmail.com>
2019-11-27 13:51:59 +0100

    profile creada la maquetacion.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
