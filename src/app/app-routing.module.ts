7b00af334592daf1fce6310c7afcc86cb85dbf18
¨Adrian <amgwebstudios@gmail.com>
2019-11-29 15:54:21 +0100

    Navbar acabado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
