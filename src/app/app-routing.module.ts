4c6783df0aab43050c9339f7ef60b753cc2016ea
Cristian Salerno <cristiansalerno18@gmail.com>
2019-11-30 12:29:04 +0100

    develop mergeado con hero y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { RegisterFieldsComponent } from './register-fields/register-fields.component';
+import { HeroComponent } from './hero/hero.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
-  { path: 'register', component: RegisterFormComponent },
-  { path: 'register/fields', component: RegisterFieldsComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HeroComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,20 @@ import { ProfileComponent } from './profile/profile.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { RegisterFieldsComponent } from './register-fields/register-fields.component';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     ProfileComponent,
     RegisterFormComponent,
     RegisterFieldsComponent
+=======
+    NavbarComponent,
+    HeroComponent
+>>>>>>> 5a6ee9f26ae35e2b4512e5b7e1c1cb76e6045433
   ],
   imports: [
     BrowserModule,
