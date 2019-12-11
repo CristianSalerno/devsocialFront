0f84d169a5cf96493d58e767485f7ecdb346c42f
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-11 16:18:48 +0100

    componente, crear nuevo proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { RegisterFormComponent } from "./register-form/register-form.component";
+import { RegisterFieldsComponent } from "./register-fields/register-fields.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HeroComponent } from "./hero/hero.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { RegisterFieldsComponent } from './register-fields/register-fields.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeroComponent } from './hero/hero.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProjectsComponent } from './projects/projects.component';
-
-import { LoginComponent } from './login/login.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { ProjectProfileComponent } from './project-profile/project-profile.component';
-
-
-
+import { LoginComponent } from "./login/login.component";
+import { EditProfileComponent } from "./edit-profile/edit-profile.component";
+import { ProjectProfileComponent } from "./project-profile/project-profile.component";
+import { NewIdeaComponent } from "./new-idea/new-idea.component";
 
 @NgModule({
   declarations: [
@@ -33,7 +30,8 @@ import { ProjectProfileComponent } from './project-profile/project-profile.compo
     ProjectsComponent,
     LoginComponent,
     EditProfileComponent,
-    ProjectProfileComponent
+    ProjectProfileComponent,
+    NewIdeaComponent
   ],
   imports: [
     BrowserModule,
@@ -41,9 +39,8 @@ import { ProjectProfileComponent } from './project-profile/project-profile.compo
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/new-idea/new-idea.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-new-idea",
+  templateUrl: "./new-idea.component.html",
+  styleUrls: ["./new-idea.component.css"]
+})
+export class NewIdeaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
