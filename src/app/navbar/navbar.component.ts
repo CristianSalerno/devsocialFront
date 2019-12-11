204dae25372193c087874d5842db6fcbe791cfce
Ricardo Esparza <ricardo.fanizzi@gmail.com>
2019-12-11 16:46:57 +0100

    update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { User } from 'models/user.model';
 import { UsersService } from 'services/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,9 +11,17 @@ import { UsersService } from 'services/users.service';
 export class NavbarComponent implements OnInit {
 
   mainUser: User;
-  constructor(private usersService: UsersService) { }
+  constructor(
+    private usersService: UsersService,
+    private router: Router) { }
 
   async ngOnInit() {
     this.mainUser = await this.usersService.mainUserExist();
+  }
+
+  async logout() {
+    localStorage.removeItem('user_token');
+    sessionStorage.removeItem('user_token');
+    await this.router.navigate(['/home']);
   }
 }
