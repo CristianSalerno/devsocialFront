1a460c48d49e5c77db5a6ce0887de81ec6995004
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-16 10:10:59 +0100

    odificado appModule y agregadas las nuevas features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { HeroComponent } from "./hero/hero.component";
 import { FooterComponent } from "./footer/footer.component";
 import { ProjectsComponent } from "./projects/projects.component";
 
-<<<<<<< HEAD
 import { LoginComponent } from "./login/login.component";
 import { EditProfileComponent } from "./edit-profile/edit-profile.component";
 import { ProjectProfileComponent } from "./project-profile/project-profile.component";
@@ -22,19 +21,9 @@ import { EditPasswordComponent } from "./edit-password/edit-password.component";
 import { EditEmailComponent } from "./edit-email/edit-email.component";
 import { ChatBoxComponent } from "./chat-box/chat-box.component";
 import { EditImageComponent } from "./edit-image/edit-image.component";
-=======
-import { LoginComponent } from './login/login.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { ProjectProfileComponent } from './project-profile/project-profile.component';
-import { NewIdeaComponent } from './new-idea/new-idea.component';
-import { EditPasswordComponent } from './edit-password/edit-password.component';
-import { EditEmailComponent } from './edit-email/edit-email.component';
-import { ChatBoxComponent } from './chat-box/chat-box.component';
-import { EditImageComponent } from './edit-image/edit-image.component';
-import { PublicProfileComponent } from './public-profile/public-profile.component';
-import { ProjectAdmComponent } from './project-adm/project-adm.component';
-import { ProjectCreatorComponent } from './project-creator/project-creator.component';
->>>>>>> b5a32ccf952d999ad6c417939e9d46cf6983bb38
+import { PublicProfileComponent } from "./public-profile/public-profile.component";
+import { ProjectAdmComponent } from "./project-adm/project-adm.component";
+import { ProjectCreatorComponent } from "./project-creator/project-creator.component";
 
 /* REDUX */
 
