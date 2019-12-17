78f660a27e41ed46ac20a4e943c865ac475bf377
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-17 18:28:44 +0100

    profile-edit

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'services/users.service';
 import { User } from 'models/user.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormControlDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
@@ -11,6 +11,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class EditProfileComponent implements OnInit {
   mainUser: User;
   updateUser: FormGroup;
+  updateUserInfo: FormGroup;
+  updateAbout: FormGroup;
+
   constructor(private usersService: UsersService) {
 
   }
@@ -21,11 +24,27 @@ export class EditProfileComponent implements OnInit {
       name: new FormControl(this.mainUser.name),
       surnames: new FormControl(this.mainUser.surnames)
     });
+    this.updateUserInfo = new FormGroup({
+      country: new FormControl(''),
+      city: new FormControl(''),
+      language: new FormControl(''),
+      phone: new FormControl('')
+    })
+    this.updateAbout = new FormGroup({
+      about: new FormControl(''),
+    })
   }
 
   async updateInfo() {
     const token = (localStorage.getItem('user_token')) ? localStorage.getItem('user_token') : sessionStorage.getItem('user_token');
     await this.usersService.updateNames(token, this.updateUser.value);
     alert('Update Sucessfull');
+  }
+
+  updateOtherInfo() {
+    console.log(this.updateUserInfo.value)
+  }
+  updateAboutInfo() {
+    console.log(this.updateAbout.value)
   }
 }
