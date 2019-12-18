ac48c983ca9bf34d63dc286c56e1c15fefb23f04
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-18 09:55:41 +0100

    editar perfil terminado

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -11,10 +11,13 @@ import { UserProfileService } from '../services/user-profile.service';
 })
 export class EditProfileComponent implements OnInit {
   mainUser: User;
+  userInfo: any;
   updateUser: FormGroup;
   updateUserInfo: FormGroup;
   updateAbout: FormGroup;
   updateSkills: FormGroup;
+  token: any;
+  id: number;
 
   constructor(
     private usersService: UsersService,
@@ -42,29 +45,35 @@ export class EditProfileComponent implements OnInit {
       name: new FormControl(this.mainUser.name),
       surnames: new FormControl(this.mainUser.surnames)
     });
+    //recupera el token del usuario logeado y lo almacena en token.
+    this.token = (localStorage.getItem('user_token')) ? localStorage.getItem('user_token') : sessionStorage.getItem('user_token');
+    this.id = parseInt((localStorage.getItem('id')) ? localStorage.getItem('id') : sessionStorage.getItem('id'));
+
+    this.getUserInfo();
+
+  }
+
+  async getUserInfo() {
+    this.userInfo = await this.userProfileService.getAllUserData(this.id);
+    console.log(this.userInfo)
   }
 
   async updateInfo() {
-    const token = (localStorage.getItem('user_token')) ? localStorage.getItem('user_token') : sessionStorage.getItem('user_token');
-    await this.usersService.updateNames(token, this.updateUser.value);
+    await this.usersService.updateNames(this.token, this.updateUser.value);
     alert('Update Sucessfull');
   }
 
   async updateOtherInfo() {
-    const token = (localStorage.getItem('user_token')) ? localStorage.getItem('user_token') : sessionStorage.getItem('user_token');
-    await this.userProfileService.editUser(token, this.updateUserInfo.value);
+    await this.userProfileService.editUser(this.token, this.updateUserInfo.value);
   }
   async updateAboutInfo() {
-    console.log(this.updateAbout.value)
-    const token = (localStorage.getItem('user_token')) ? localStorage.getItem('user_token') : sessionStorage.getItem('user_token');
-    await this.userProfileService.updateAbout(token, this.updateAbout.value);
+    await this.userProfileService.updateAbout(this.token, this.updateAbout.value);
   }
 
   async updateSkill() {
-    console.log(this.updateSkills.value)
-    const token = (localStorage.getItem('user_token')) ? localStorage.getItem('user_token') : sessionStorage.getItem('user_token');
-    await this.userProfileService.updateSkills(token, this.updateSkills.value)
+    await this.userProfileService.updateSkills(this.token, this.updateSkills.value)
   }
+
 
 
 }
